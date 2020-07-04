5a236b75f972b81c22d4fecbd5d287296a639b7a
Robert Alestra <bobbyalestra135@gmail.com>
2020-07-04 18:30:28 -0400

    did more work on react todo

--- moz-todo-react/src/index.js
+++ moz-todo-react/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App subject="Clarice" />, 
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
