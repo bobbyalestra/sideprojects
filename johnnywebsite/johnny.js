356d8854090332c7c53630c6ec78336d218f343e
Robert Alestra <bobbyalestra135@gmail.com>
2020-10-31 15:46:38 -0400

    making div for Johnn y website

--- johnnywebsite/johnny.js
+++ johnnywebsite/johnny.js
@@ -5,4 +5,5 @@ document.addEventListener("DOMContent Loaded",() => {
 let aboutMeDiv = document.createElement("div");
 aboutMeDiv.id = "aboutMe";
 aboutMeDiv.innerText = " My name is Johnny Yarbrough, I have been playing hockey for the past 9 years. I enjoy playing all sports and hav fun doing so.  I'm competive and always try my hardest and be the best team player. Check out some of my highlighted videos."
+document.body.appendChild(aboutMeDiv);
 })
