4dfb31e25e42ff5b422b2f69d16e9327a9c5a01e
Robert Alestra <bobbyalestra135@gmail.com>
2021-01-26 09:36:34 -0500

    adding routes to exercise app

---
+++ exerciseTracker/mern-exercise-tracker/backend/routes/dist/exercise.route.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var Exercise = require("../../../../../../../../../../sideprojects/exerciseTracker/mern-exercise-tracker/backend/models/user.model");
+
+var router = require('express').Router();
+
+var User = require('../models/user.model');
+
+router.route('/').get(function (req, res) {
+  User.find().then(function (users) {
+    return res.json(users);
+  })["catch"](function (err) {
+    return res.status(400).json('Error: ' + err);
+  });
+});
+router.route('/add').post(function (req, res) {
+  var username = req.body.username;
+  var newUser = new User({
+    username: username
+  });
+  newUser.save().then(function () {
+    return res.json('User added!');
+  })["catch"](function (err) {
+    return res.status(400).json('Error: ' + err);
+  });
+});
+module.exports = router;
