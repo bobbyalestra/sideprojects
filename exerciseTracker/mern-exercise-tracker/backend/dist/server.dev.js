ba66fe908e26d4ec9f5cdccb549ab24e8a19e9bc
Robert Alestra <bobbyalestra135@gmail.com>
2021-01-26 09:56:20 -0500

    making components

--- exerciseTracker/mern-exercise-tracker/backend/dist/server.dev.js
+++ exerciseTracker/mern-exercise-tracker/backend/dist/server.dev.js
@@ -12,15 +12,22 @@ var app = express();
 var port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.listen(process.env.ATLAS_URI);
-mongoose.connect('mongodb://localhost/test'), {
+var uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-};
+  useCreateIndex: true
+});
 var connection = mongoose.connection;
 connection.once('open', function () {
   console.log("MongoDB database connection established successfully");
 });
+
+var exercisesRouter = require('./backend/routes/exercises');
+
+var usersRouter = require('./backend/routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 app.listen(port, function () {
   console.log("Server is running on port: ".concat(port));
 });
