f70df347cce1a62ac09c065cf654c424cc4b1776
Bobby Alestra <bobbyalestra135@gmail.com>
2021-09-19 13:33:14 -0400

    updates with notes

---
+++ random/practice.js
@@ -0,0 +1,12 @@
+
+
+let string = "This is a string";
+
+for (let i = 0; i < string.length; i++) {
+
+    //tell the number length
+    console.log(i)
+
+    // each indivudual letter
+    console.log(string[i])
+}
