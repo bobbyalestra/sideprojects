163c738258c50796dab08334186e48e53df9a460
Bobby Alestra <bobbyalestra135@gmail.com>
2021-09-29 17:58:59 -0400

    game with paddles

--- python_tutorial/Pong1.py
+++ python_tutorial/Pong1.py
@@ -12,16 +12,21 @@ wn.tracer(0)
 
 # Paddle A
 paddle_a = turtle.Turtle()
-
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 
 
 # PADDLE B
-
+paddle_b = turtle.Turtle()
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 #BALL
 
 
