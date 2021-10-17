b9ad970b4d0580521af3266d137df688a0e571a4
Bobby Alestra <bobbyalestra135@gmail.com>
2021-10-17 13:41:49 -0400

    More work ith buttons in tkinter

--- pythonTkinter/radiobuttons.py
+++ pythonTkinter/radiobuttons.py
@@ -7,13 +7,31 @@ root= Tk()
 root.title("Radio Button")
 
 r = IntVar()
+r.set("2")
 
 
-Radiobutton(root, text=" Option 1", variable=r, value=1).pack()
-Radiobutton(root, text=" Option 2", variable=r, value=2).pack()
+MODES = [
+    ("Pepperoni", "Pepperoni"),
+    ("Cheese", "Cheese"),
+    ("Mushroom", "Mushroom"),
+    ("Onion", "Onion"),
+]
+
+
+def clicked(value):
+    myLabel = Label(root, text=value)
+    myLabel.pack()    
+
+#Radiobutton(root, text=" Option 1", variable=r, value=1, command= lambda: clicked(r.get())).pack()
+#Radiobutton(root, text=" Option 2", variable=r, value=2, command= lambda: clicked(r.get())).pack()
 #RadioButton(root, text=" Option 3", variable=r, value=3).pack()
 #RadioButton(root, text=" Option 4", variable=r, value=4).pack()
 
 
+myLabel = Label(root, text=r.get())
+myLabel.pack()
+
+myButton =Button(root, text="click me",command= lambda: clicked(r.get()))
+myButton.pack()
 
 mainloop()
