35df49dc5bb6f3294355a72e4d74d73dc0b231c2
Bobby Alestra <bobbyalestra135@gmail.com>
2021-10-17 12:35:57 -0400

    adding subtract, multiply, and divide buttons to calculator

--- pythonTkinter/calc.py
+++ pythonTkinter/calc.py
@@ -44,6 +44,9 @@ button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
 button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
 button_clear = Button(root, text="clear", padx=79, pady=20, command= button_clear)
 
+button_subtract = Button(root, text="-", padx=39, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=39, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=39, pady=20, command=button_divide)
 # putting buttons on screen
 
 button_1.grid(row=3, column=0 )
@@ -62,6 +65,10 @@ button_0.grid(row=4, column=0 )
 button_clear.grid(row=4, column=1,columnspan=21)
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 root.mainloop()
 
